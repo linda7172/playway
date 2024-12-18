d221828e0be8d71f0ce3643f397eba89544817b5
linda7172 <116900968+linda7172@users.noreply.github.com>
2024-12-18 09:00:54 +0000

    add in alert for 2 consective incorrect answers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
-// import { Button } from "@mui/material";
 import React, { useState } from "react";
-// import ProgressMobileStepper from "./components/progressmobilestepper";
-import Retake from "./components/retake";
+import Retake from "../components/retake";
 
 type Option = {
   option: string;
@@ -15,70 +13,110 @@ type Question = {
   options: Option[];
 }
 
-const questions: Question[] = [{
-  id: 1,
-  question: "What is the capital of France?",
-  options: [
-    { option: "Paris", isCorrect: true },
-    { option: "Berlin", isCorrect: false },
-    { option: "Madrid", isCorrect: false },
-    { option: "London", isCorrect: false }]
-}, {
-  id: 2,
-  question: "What is the captial of Germany?",
-  options: [
-    { option: "Paris", isCorrect: false },
-    { option: "Berlin", isCorrect: true },
-    { option: "Madrid", isCorrect: false },
-    { option: "London", isCorrect: false }]
-}, {
-  id: 3,
-  question: "What is the capital of Spain?",
-  options: [
-    { option: "Paris", isCorrect: false },
-    { option: "Berlin", isCorrect: false },
-    { option: "Madrid", isCorrect: true },
-    { option: "London", isCorrect: false }]
-}, {
-  id: 4,
-  question: "The capital of Italy is London.",
-  options: [
-    { option: "True", isCorrect: false },
-    { option: "False", isCorrect: true }]
-}, {
-  id: 5,
-  question: "What is the capital of England?",
-  options: [
-    { option: "Paris", isCorrect: false },
-    { option: "Berlin", isCorrect: false },
-    { option: "Madrid", isCorrect: false },
-    { option: "London", isCorrect: true }]
-}];
+const questions: Question[] = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    options: [
+      { option: "Paris", isCorrect: true },
+      { option: "Berlin", isCorrect: false },
+      { option: "Madrid", isCorrect: false },
+      { option: "London", isCorrect: false }
+    ]
+  },
+  {
+    id: 2,
+    question: "What is the capital of Germany?",
+    options: [
+      { option: "Paris", isCorrect: false },
+      { option: "Berlin", isCorrect: true },
+      { option: "Madrid", isCorrect: false },
+      { option: "London", isCorrect: false }
+    ]
+  },
+  {
+    id: 3,
+    question: "What is the capital of Spain?",
+    options: [
+      { option: "Paris", isCorrect: false },
+      { option: "Berlin", isCorrect: false },
+      { option: "Madrid", isCorrect: true },
+      { option: "London", isCorrect: false }
+    ]
+  },
+  {
+    id: 4,
+    question: "The capital of Italy is London.",
+    options: [
+      { option: "True", isCorrect: false },
+      { option: "False", isCorrect: true }
+    ]
+  },
+  {
+    id: 5,
+    question: "What is the capital of England?",
+    options: [
+      { option: "Paris", isCorrect: false },
+      { option: "Berlin", isCorrect: false },
+      { option: "Madrid", isCorrect: false },
+      { option: "London", isCorrect: true }
+    ]
+  },
+  {
+    id: 5,
+    question: "What is the capital of England?",
+    options: [
+      { option: "Paris", isCorrect: false },
+      { option: "Berlin", isCorrect: false },
+      { option: "Madrid", isCorrect: false },
+      { option: "London", isCorrect: true }
+    ]
+  },
+  {
+    id: 5,
+    question: "What is the capital of England?",
+    options: [
+      { option: "Paris", isCorrect: false },
+      { option: "Berlin", isCorrect: false },
+      { option: "Madrid", isCorrect: false },
+      { option: "London", isCorrect: true }
+    ]
+  }
+];
 
 export default function Home() {
-  //start quiz from question 0
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   const [showScore, setShowScore] = useState<boolean>(false);
   const [previousIncorrect, setPreviousIncorrect] = useState<boolean>(false);
   const [score, setScore] = useState<number>(0);
+  const [retakeAlert, setRetakeAlert] = useState<boolean>(false);
 
   const handleAnswerOptionClick = (isCorrect: boolean) => {
-    //if correct answer, increase score by 1, if not, set previousIncorrect to incorrect -> true 
+    // If correct answer, increase score by 1, if not, set previousIncorrect to true
+    setRetakeAlert(false);
     if (isCorrect) {
       setScore(score + 1);
+      setPreviousIncorrect(false); // reset the incorrect flag if the answer is correct
     } else {
       setPreviousIncorrect(true);
     }
-    if (previousIncorrect === true && !isCorrect) {
-      setShowScore(true);
-      <Retake />;
+
+    if (previousIncorrect && !isCorrect) {
+      // Show the Retake component when two incorrect answers are selected
+      setRetakeAlert(true);
+      setPreviousIncorrect(false);
     }
+
     const nextQuestion = currentQuestion + 1;
+
+
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
+
     } else {
       setShowScore(true);
     }
+
   };
 
   return (
@@ -93,6 +131,9 @@ export default function Home() {
             <div className='question-count'>
               <span>Question {currentQuestion + 1}</span>/{questions.length}
             </div>
+            {/* onClose function takes on the value of retake */}
+            {retakeAlert && <Retake onClose={() => setRetakeAlert(false)} />}
+
             <div className='question-text'>{questions[currentQuestion].question}</div>
           </div>
           <div className='answer-section'>
@@ -107,4 +148,3 @@ export default function Home() {
     </div>
   );
 }
-
---
+++ src/components/retake.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
+
+export interface Props {
+    onClose: () => void; // Function that closes the alert
+}
+
+export default function TransitionAlerts(props: Props) {
+    const { onClose } = props;
+
+    return (
+        <Box sx={{ width: '40%' }}>
+            <Collapse in={true}>
+                <Alert severity="info"
+                    action={
+                        <IconButton
+                            aria-label="close"
+                            color="inherit"
+                            size="small"
+                            onClick={onClose}
+                        >
+                            <CloseIcon fontSize="inherit" />
+                        </IconButton>
+                    }
+                    sx={{ mb: 2 }}
+                >
+                    You have 2 consecutive wrong answers. You may want to rewatch the video course and try again.
+                </Alert>
+            </Collapse>
+
+        </Box>
+    );
+}
