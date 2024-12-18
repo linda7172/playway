3633f14ae15239b685eef9df2f9e4f0539999402
linda7172 <116900968+linda7172@users.noreply.github.com>
2024-12-18 07:29:47 +0000

    quiz skeleton

---
+++ src/app/components/retake.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function Retake() {
+
+    return (
+        <div>
+            <h1>You scored 2 incorrect answers in a row. Please revise the video course and try again.</h1>
+        </div>
+
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,110 @@
-import Image from "next/image";
+"use client";
+// import { Button } from "@mui/material";
+import React, { useState } from "react";
+// import ProgressMobileStepper from "./components/progressmobilestepper";
+import Retake from "./components/retake";
+
+type Option = {
+  option: string;
+  isCorrect: boolean;
+};
+
+type Question = {
+  id: number;
+  question: string;
+  options: Option[];
+}
+
+const questions: Question[] = [{
+  id: 1,
+  question: "What is the capital of France?",
+  options: [
+    { option: "Paris", isCorrect: true },
+    { option: "Berlin", isCorrect: false },
+    { option: "Madrid", isCorrect: false },
+    { option: "London", isCorrect: false }]
+}, {
+  id: 2,
+  question: "What is the captial of Germany?",
+  options: [
+    { option: "Paris", isCorrect: false },
+    { option: "Berlin", isCorrect: true },
+    { option: "Madrid", isCorrect: false },
+    { option: "London", isCorrect: false }]
+}, {
+  id: 3,
+  question: "What is the capital of Spain?",
+  options: [
+    { option: "Paris", isCorrect: false },
+    { option: "Berlin", isCorrect: false },
+    { option: "Madrid", isCorrect: true },
+    { option: "London", isCorrect: false }]
+}, {
+  id: 4,
+  question: "The capital of Italy is London.",
+  options: [
+    { option: "True", isCorrect: false },
+    { option: "False", isCorrect: true }]
+}, {
+  id: 5,
+  question: "What is the capital of England?",
+  options: [
+    { option: "Paris", isCorrect: false },
+    { option: "Berlin", isCorrect: false },
+    { option: "Madrid", isCorrect: false },
+    { option: "London", isCorrect: true }]
+}];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  //start quiz from question 0
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const [showScore, setShowScore] = useState<boolean>(false);
+  const [previousIncorrect, setPreviousIncorrect] = useState<boolean>(false);
+  const [score, setScore] = useState<number>(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleAnswerOptionClick = (isCorrect: boolean) => {
+    //if correct answer, increase score by 1, if not, set previousIncorrect to incorrect -> true 
+    if (isCorrect) {
+      setScore(score + 1);
+    } else {
+      setPreviousIncorrect(true);
+    }
+    if (previousIncorrect === true && !isCorrect) {
+      setShowScore(true);
+      <Retake />;
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <div className='app'>
+      {showScore ? (
+        <div className='score-section'>
+          You scored {score} out of {questions.length}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ) : (
+        <>
+          <div className='question-section'>
+            <div className='question-count'>
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className='question-text'>{questions[currentQuestion].question}</div>
+          </div>
+          <div className='answer-section'>
+            {questions[currentQuestion].options.map((option, index) => (
+              <button key={index} onClick={() => handleAnswerOptionClick(option.isCorrect)}>
+                {option.option}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
+
